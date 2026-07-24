065e89641596cf636796af82efba4953415a2abc
ekabotdev <ekabotdev@gmail.com>
2026-07-24 20:45:42 +0300

    feat: add User entity

---
+++ src/main/java/com/ekabotdev/taskmanager/entity/User.java
@@ -0,0 +1,54 @@
+package com.ekabotdev.taskmanager.entity;
+
+
+import com.ekabotdev.taskmanager.enums.Role;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name= "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(min = 3, max = 50)
+    @Column(nullable = false ,unique = true, length = 50)
+    private String username;
+
+    @NotBlank
+    @Email
+    @Column(nullable = false, unique = true, length = 255)
+    private String email;
+
+    @NotBlank
+    @Size(min = 8, max = 60)
+    @Column(nullable = false, length = 60)
+    private String password;
+
+    @NotNull
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
