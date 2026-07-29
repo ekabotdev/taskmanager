5293073434a4dcbe330a4b5da967fb019c63db6c
ekabotdev <ekabotdev@gmail.com>
2026-07-29 12:21:01 +0300

    feat: add task entity class

---
+++ src/main/java/com/ekabotdev/taskmanager/entity/Task.java
@@ -0,0 +1,59 @@
+package com.ekabotdev.taskmanager.entity;
+
+import com.ekabotdev.taskmanager.enums.Priority;
+import com.ekabotdev.taskmanager.enums.Status;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import lombok.*;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 100 ,message = "Title must be under 100 characters")
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @NotBlank
+    @Size(max = 1000, message = "Description must be under 1000 characters")
+    @Column(nullable = false, length = 1000)
+    private String description;
+
+    @NotNull
+    @Column(nullable = false, length = 20)
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @NotNull
+    @Column(nullable = false, length = 20)
+    @Enumerated(EnumType.STRING)
+    private Priority priority;
+
+    @NotNull
+    @Column( name = "due_date",nullable = false)
+    private LocalDateTime dueDate;
+
+    @Column(name = "created_at",nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column( name = "updated_at",nullable = false)
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/com/ekabotdev/taskmanager/enums/Priority.java
@@ -0,0 +1,8 @@
+package com.ekabotdev.taskmanager.enums;
+
+public enum Priority {
+    LOW,
+    MEDIUM,
+    HIGH,
+    ARGENT
+}
